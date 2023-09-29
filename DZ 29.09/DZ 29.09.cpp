70fd12d2e8fcbf48f3c7f25a9af93e909fd3dad8
Sasha <alecsand.unit@gmail.com>
2023-09-29 17:26:55 +0300

    приложение «Валидатор HTML-файлов».

--- DZ 29.09/DZ 29.09.cpp
+++ DZ 29.09/DZ 29.09.cpp
@@ -29,7 +29,7 @@ int main()
         }
 
         cout << "Quantity '<': " << symbolLessThan << endl;
-        cout << "Quantity '>': " << symbolGreaterThan << endl;
+        cout << "Quantity '>': " << symbolGreaterThan << endl; 
         
         int temp = symbolGreaterThan;    
 
